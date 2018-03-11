61a14d3196fb9b181adf6dc1f275d8144815d86e
JOB <getabujob@gmail.com>
2018-03-12 00:15:46 +0300

    added assets and icons

---
+++ app/src/main/java/com/job/onboardingapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.job.onboardingapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by JOB on 3/11/2018.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
